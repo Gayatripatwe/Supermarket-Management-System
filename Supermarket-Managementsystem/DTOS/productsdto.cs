59ec87e22056678556d2fc8ab673a06a8027f3bc
gayatri <patwegayatri5143@gmail.com>
2024-09-20 15:42:40 +0530

    renaming to supermarketmanagement system

---
+++ Supermarket-Managementsystem/DTOS/productsdto.cs
@@ -0,0 +1,11 @@
+﻿namespace Supermarket_Managementsystem.DTOS
+{
+    public class productsdto
+    {
+        public string name { get; set; } = String.Empty;
+        public int quantity { get; set; } = 0;
+        public double price { get; set; }
+
+        public int categoryid { get; set; }
+    }
+}
---
+++ Supermarket-Managementsystem/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket_Managementsystem.Models
+{
+    public class Order
+    {
+        [Key]
+        public int id { get; set; }
+        public double totalprice { get; set; }
+        public string date { get; set; }
+        public int Customerid { get; set; }
+        public Customer ?customer { get; set; }
+         public ICollection<Orderitem> ?orderitems { get; set; }
+
+
+    }
+}
